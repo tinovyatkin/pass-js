e52529cef1cac68e850a49f7e525d6a0848328c4
tinovyatkin <tino@vtkn.io>
2017-08-08 12:45:10 -0400

    created `index.js` to reexport major classes and constants

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Template = require('./src/template');
+const Pass = require('./src/pass');
+const constants = require('./src/constants');
+
+module.exports = {
+  Template,
+  Pass,
+  constants,
+};
