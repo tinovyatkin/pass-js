32db143f3c2c84210da7ccf5f4bfe540cb88211a
Tino Vyatkin <tino@vtkn.io>
2019-05-17 16:10:53 -0400

    remove fast-glob

--- __tests__/images.ts
+++ __tests__/images.ts
@@ -2,9 +2,30 @@
 
 import * as path from 'path';
 
-import { PassImages } from '../src/lib/images';
+import { PassImages, IMAGE_FILENAME_REGEX } from '../src/lib/images';
 
 describe('PassImages', () => {
+  it('IMAGE_FILENAME_REGEX', () => {
+    expect('logo.png').toMatch(IMAGE_FILENAME_REGEX);
+    expect('icon@2x.png').toMatch(IMAGE_FILENAME_REGEX);
+    expect('thumbnail@3x.png').toMatch(IMAGE_FILENAME_REGEX);
+    expect('logo@4x').not.toMatch(IMAGE_FILENAME_REGEX);
+    expect('byaka.png').not.toMatch(IMAGE_FILENAME_REGEX);
+    expect('logo.jpg').not.toMatch(IMAGE_FILENAME_REGEX);
+  });
+
+  it('parseFilename', () => {
+    const img = new PassImages();
+    expect(img.parseFilename('logo.png')).toEqual({
+      imageType: 'logo',
+    });
+    expect(img.parseFilename('icon@2x.png')).toEqual({
+      imageType: 'icon',
+      density: '2x',
+    });
+    expect(img.parseFilename('logo.jpg')).toBeUndefined();
+  });
+
   it('has class properties', () => {
     const img = new PassImages();
     expect(img.load).toBeInstanceOf(Function);
